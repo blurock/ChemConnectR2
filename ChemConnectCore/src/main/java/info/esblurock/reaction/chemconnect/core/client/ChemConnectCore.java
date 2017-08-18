60eee3aa5c6d1f8c0270b3919defb4e4d7b62365
edwardblurock <edward.blurock@gmail.com>
2017-08-18 14:34:24 +0200

    Initial GWT compile for ChemConnectCore
    deleted GWT generated files from ChemConnectCoreData

--- ChemConnectCore/src/main/java/info/esblurock/reaction/chemconnect/core/client/ChemConnectCore.java
+++ ChemConnectCore/src/main/java/info/esblurock/reaction/chemconnect/core/client/ChemConnectCore.java
@@ -1,11 +1,13 @@
 package info.esblurock.reaction.chemconnect.core.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 
 public class ChemConnectCore implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
+		Window.alert("Called Module");
 	}
 
 }
