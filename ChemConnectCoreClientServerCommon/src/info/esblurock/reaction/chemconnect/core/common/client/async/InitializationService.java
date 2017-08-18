edd5030502ea3ded2e7d61f66c4bae32adb13f4d
edwardblurock <edward.blurock@gmail.com>
2017-08-18 14:46:32 +0200

    Initial successful run of app engine (entry point empty)

--- ChemConnectCoreClientServerCommon/src/info/esblurock/reaction/chemconnect/core/common/client/async/InitializationService.java
+++ ChemConnectCoreClientServerCommon/src/info/esblurock/reaction/chemconnect/core/common/client/async/InitializationService.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+@RemoteServiceRelativePath("initialization")
 public interface InitializationService extends RemoteService {
 	   public static class Util
 	   {
